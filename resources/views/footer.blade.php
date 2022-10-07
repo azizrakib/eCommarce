9d0d8d78456905362d27b084ae64d55aca2528c6
azizrakib <aziz.cse191@gmail.com>
2022-10-08 01:45:19 +0600

    header, footer and login done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,20 @@
+<div class="bg-secondary  w-100 mt-3" style="--bs-bg-opacity: .5; height: 200px;">
+<div class="row">
+    <div class="col-sm-4">
+        Column 1
+    </div>
+    <div class="col-sm-2">
+        Column 2
+    </div>
+    <div class="col-sm-2">
+        Column 3
+    </div>
+    <div class="col-sm-2">
+        Column 4
+    </div>
+    <div class="col-sm-2">
+        Column 5
+    </div>
+</div>
+ 
+</div>
